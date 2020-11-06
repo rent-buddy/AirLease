c7bc430c8a20dc8fca897cb078502edf86f316b6
Michelle24211 <Michelle24211@gmail.com>
2020-11-06 17:29:56 -0500

    Database add Item, Transaction, User

--- client/src/components/Login/Registration.js
+++ client/src/components/Login/Registration.js
@@ -1,44 +1,44 @@
 import React, { Component } from 'react';
-import {Button} from 'react-bootstrap';
+import { Button } from 'react-bootstrap';
 import Login from './Login';
 import SignUp from './SignUp';
-class Registration extends Component{
-  state = {
-    loginOpened: false,
-    signupOpened: false
-  };
+class Registration extends Component {
+    state = {
+          loginOpened: false,
+        signupOpened: false,
+    };
 
-  openModal = modalType => () => {
-    if (modalType === "login") {
-      this.setState({
-        loginOpened: true,
-        signupOpened: false
-      });
-    } else if (modalType === "signup") {
-      this.setState({
-        loginOpened: false,
-        signupOpened: true
-      });
+    openModal = (modalType) => () => {
+        if (modalType === 'login') {
+            this.setState({
+                loginOpened: true,
+                signupOpened: false,
+            });
+        } else if (modalType === 'signup') {
+            this.setState({
+                loginOpened: false,
+                signupOpened: true,
+            });
+        }
+    };
+
+    closeModal = (modalType) => () => {
+        this.setState({
+            loginOpened: false,
+            signupOpened: false,
+        });
+    };
+
+    render() {
+        const { loginOpened, signupOpened } = this.state;
+        return (
+            <>
+                <Button onClick={this.openModal('login')}> Login/Sign Up</Button>
+                <Login show={loginOpened} onRequestClose={this.closeModal()} onClick={this.openModal('signup')} />
+                <SignUp show={signupOpened} onRequestClose={this.closeModal()} onClick={this.openModal('login')} />
+            </>
+        );
     }
-  };
-
-  closeModal = modalType => () => {
-    this.setState({
-      loginOpened: false,
-      signupOpened: false
-    });
-  };
-  
-  render() {
-    const { loginOpened, signupOpened } = this.state;
-    return (
-      <>
-        <Button onClick = {this.openModal("login")}> Login/Sign Up</Button>
-        <Login show = {loginOpened} onRequestClose = {this.closeModal()} onClick={this.openModal("signup")}/>
-        <SignUp show = {signupOpened} onRequestClose = {this.closeModal()} onClick={this.openModal("login")}/>
-      </>
-    );
-  }
 }
 
 export default Registration;
