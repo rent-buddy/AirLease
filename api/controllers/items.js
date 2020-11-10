42f2027d3587a8ed8e8ed41919215efd0322072c
Anton Zabirko <work@azabirko.com>
2020-11-10 16:00:03 -0500

    Created items API controller.

---
+++ api/controllers/items.js
@@ -0,0 +1,70 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../models');
+const { Item } = db;
+
+//    GET    /items
+//    POST   /items
+//    GET    /items/:id
+//    PUT    /items/:id
+//    DELETE /items/:id
+
+router.get('/', (req, res) => {
+  Item.findAll({}).then((items) => res.json(items));
+});
+
+router.post('/', (req, res) => {
+  let { content } = req.body;
+
+  Item.create({ content })
+    .then((item) => {
+      res.status(201).json(item);
+    })
+    .catch((err) => {
+      res.status(400).json(err);
+    });
+});
+
+router.get('/:id', (req, res) => {
+  const { id } = req.params;
+
+  Item.findByPk(id).then((item) => {
+    if (!item) {
+      return res.sendStatus(404);
+    }
+    res.json(item);
+  });
+});
+
+router.put('/:id', (req, res) => {
+  const { id } = req.params;
+  Item.findByPk(id).then((item) => {
+    if (!item) {
+      return res.sendStatus(404);
+    }
+
+    item.content = req.body.content;
+    item
+      .save()
+      .then((item) => {
+        res.json(item);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  });
+});
+
+router.delete('/:id', (req, res) => {
+  const { id } = req.params;
+  Item.findByPk(id).then((item) => {
+    if (!item) {
+      return res.sendStatus(404);
+    }
+
+    item.destroy();
+    res.sendStatus(204);
+  });
+});
+
+module.exports = router;
