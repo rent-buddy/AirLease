d35d372c9f247fe9a7a9e7fbc022bfb2a1f1e630
Michelle24211 <42896533+Michelle24211@users.noreply.github.com>
2020-11-20 14:43:39 -0500

    Update index.js

--- api/controllers/index.js
+++ api/controllers/index.js
@@ -2,13 +2,13 @@ const express = require('express');
 const router = express.Router();
 
 // Load each controller
-//const authController = require('./auth');
+const authController = require('./auth');
 const postsController = require('./posts.js');
 const appConfigController = require('./appConfig.js');
 
 // Mount each controller under a specific route. These
 // will be prefixes to all routes defined inside the controller
-//router.use('/auth', authController);
+router.use('/auth', authController);
 router.use('/posts', postsController);
 router.use('/application-configuration', appConfigController);
 
