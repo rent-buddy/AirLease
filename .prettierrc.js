df1454f8d1a4074cfcfe80c3e60f213b63b20bcf
kwu25 <kelvin.wu125@gmail.com>
2020-12-02 18:54:13 -0500

    Fixed prettierrc

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,16 +1,7 @@
 module.exports = {
-<<<<<<< Updated upstream
-	semi: true,
-	trailingComma: "all",
-	singleQuote: true,
-	printWidth: 120,
-	tabWidth: 2
-};
-=======
     semi: true,
     trailingComma: 'all',
     singleQuote: true,
     printWidth: 120,
     tabWidth: 2,
 };
->>>>>>> Stashed changes
